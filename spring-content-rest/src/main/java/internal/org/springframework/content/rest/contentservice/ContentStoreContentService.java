5c5740883b098516f6c1e1957fa7d666d3c13724
Vladimir Svyatski <v.svyatski@gmail.com>
2026-09-17 16:39:11 +0300

    Clean code

--- spring-content-rest/src/main/java/internal/org/springframework/content/rest/contentservice/ContentStoreContentService.java
+++ spring-content-rest/src/main/java/internal/org/springframework/content/rest/contentservice/ContentStoreContentService.java
@@ -20,7 +20,6 @@ import org.springframework.content.commons.repository.ContentStore;
 import org.springframework.content.commons.repository.SetContentParams;
 import org.springframework.content.commons.repository.Store;
 import org.springframework.content.commons.repository.UnsetContentParams;
-import org.springframework.content.commons.storeservice.StoreInfo;
 import org.springframework.content.commons.utils.StoreInterfaceUtils;
 import org.springframework.content.rest.RestResource;
 import org.springframework.content.rest.config.RestConfiguration;
@@ -60,7 +59,7 @@ public class ContentStoreContentService implements ContentService {
     private final ContentPropertyToExportedContext exportContext;
     private final StoreByteRangeHttpRequestHandler byteRangeRestRequestHandler;
 
-    public ContentStoreContentService(RestConfiguration config, StoreInfo store, RepositoryInvoker repoInvoker, MappingContext mappingContext, ContentPropertyToExportedContext exportContext, StoreByteRangeHttpRequestHandler byteRangeRestRequestHandler) {
+    public ContentStoreContentService(RestConfiguration config, RepositoryInvoker repoInvoker, MappingContext mappingContext, ContentPropertyToExportedContext exportContext, StoreByteRangeHttpRequestHandler byteRangeRestRequestHandler) {
         this.config = config;
         this.repoInvoker = repoInvoker;
         this.mappingContext = mappingContext;
@@ -68,12 +67,34 @@ public class ContentStoreContentService implements ContentService {
         this.byteRangeRestRequestHandler = byteRangeRestRequestHandler;
     }
 
+    public static StoreExportedMethodsMap getExportedMethodsFor(Class<? extends Store> storeInterfaceClass, PropertyPath path, ContentPropertyToExportedContext exportContext) {
+
+        String key = storeInterfaceClass.getCanonicalName() + "#" + path.toString();
+        StoreExportedMethodsMap exportMap = storeExportedMethods.get(key);
+        if (exportMap == null) {
+            storeExportedMethods.put(key, new StoreExportedMethodsMap(storeInterfaceClass, path, exportContext));
+            exportMap = storeExportedMethods.get(key);
+        }
+
+        return exportMap;
+    }
+
+    public static boolean isClientAbortException(Exception e) {
+        // prior to Spring Boot 3.2.4
+        if (e.getClass().getSimpleName().equals("ClientAbortException")) {
+            return true;
+            // Spring Boot >= 3.2.4
+        } else if (e instanceof org.springframework.web.context.request.async.AsyncRequestNotUsableException) {
+            return e.getCause().getClass().getSimpleName().equals("ClientAbortException");
+        }
+        return false;
+    }
+
     @Override
     public void getContent(HttpServletRequest request, HttpServletResponse response, HttpHeaders headers, Resource resource, MediaType resourceType)
             throws ResponseStatusException, MethodNotAllowedException {
 
-        AssociatedStoreResource storeResource = (AssociatedStoreResource) resource;
-        ContentProperty property = storeResource.getContentProperty();
+        var storeResource = (AssociatedStoreResource) resource;
 
         Method[] methodsToUse = getExportedMethodsFor(storeResource.getStoreInfo().getInterface(), storeResource.getPropertyPath(), exportContext).getContentMethods();
 
@@ -133,9 +154,7 @@ public class ContentStoreContentService implements ContentService {
         try {
             byteRangeRestRequestHandler.handleRequest(request, response);
         } catch (Exception e) {
-            if (isClientAbortException(e)) {
-                // suppress
-            } else {
+            if (!isClientAbortException(e)) {
                 logger.error("Unable to handle request", e);
 
                 throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, format("Failed to handle request for %s", resource.getDescription()), e);
@@ -150,7 +169,7 @@ public class ContentStoreContentService implements ContentService {
             throw new BadRequestException("Missing Content-Type header");
         }
 
-        AssociatedStoreResource storeResource = (AssociatedStoreResource) target;
+        var storeResource = (AssociatedStoreResource) target;
         ContentProperty property = storeResource.getContentProperty();
 
         // Update mimeType and originalFilename before setContent-method invocation,
@@ -227,7 +246,7 @@ public class ContentStoreContentService implements ContentService {
     @Override
     public void unsetContent(Resource resource) throws MethodNotAllowedException {
 
-        AssociatedStoreResource storeResource = (AssociatedStoreResource) resource;
+        var storeResource = (AssociatedStoreResource) resource;
         ContentProperty property = storeResource.getContentProperty();
 
         Method[] methodsToUse = getExportedMethodsFor(storeResource.getStoreInfo().getInterface(), storeResource.getPropertyPath(), exportContext).unsetContentMethods();
@@ -255,9 +274,7 @@ public class ContentStoreContentService implements ContentService {
 
         ReflectionUtils.makeAccessible(methodsToUse[0]);
 
-        Object updatedDomainObj = ReflectionUtils.invokeMethod(methodsToUse[0], targetObj, updateObject, storeResource.getPropertyPath(), unsetParams);
-
-        updateObject = updatedDomainObj;
+        updateObject = ReflectionUtils.invokeMethod(methodsToUse[0], targetObj, updateObject, storeResource.getPropertyPath(), unsetParams);
         property.setMimeType(updateObject, null);
         property.setOriginalFileName(updateObject, null);
 
@@ -281,7 +298,7 @@ public class ContentStoreContentService implements ContentService {
             try {
                 return resource.getInputStream();
             } catch (IOException e) {
-                throw new IllegalArgumentException(format("Unable to get inputstream from resource %s", resource.getFilename()));
+                throw new IllegalArgumentException(format("Unable to get input stream from resource %s", resource.getFilename()));
             }
         } else if (Resource.class.equals(parameterType)) {
             try {
@@ -349,11 +366,11 @@ public class ContentStoreContentService implements ContentService {
                 try {
                     MediaType candidateType = MediaType.parseMediaType(strCandidateMimeType);
                     if (acceptedMimeType.includes(candidateType) && matchParameters(acceptedMimeType, candidateType)) {
-                        ContentStore store = storeResource.getStoreInfo().getImplementation(ContentStore.class);
+                        var store = storeResource.getStoreInfo().getImplementation(ContentStore.class);
                         storedRenditionResource = new RenderedResource(store.getContent(entity, PropertyPath.from(contentProperty.getContentPropertyPath())), storeResource);
                         break;
                     }
-                } catch (InvalidMediaTypeException imte) {
+                } catch (InvalidMediaTypeException ignored) {
                 }
             }
         }
@@ -361,66 +378,53 @@ public class ContentStoreContentService implements ContentService {
         return storedRenditionResource;
     }
 
-    public static StoreExportedMethodsMap getExportedMethodsFor(Class<? extends Store> storeInterfaceClass, PropertyPath path, ContentPropertyToExportedContext exportContext) {
-
-        StoreExportedMethodsMap exportMap = storeExportedMethods.get(storeInterfaceClass.getCanonicalName() + "#" + path.toString());
-        if (exportMap == null) {
-            storeExportedMethods.put(storeInterfaceClass.getCanonicalName() + "#" + path.toString(), new StoreExportedMethodsMap(storeInterfaceClass, path, exportContext));
-            exportMap = storeExportedMethods.get(storeInterfaceClass.getCanonicalName() + "#" + path.toString());
-        }
-
-        return exportMap;
-    }
-
     public static class StoreExportedMethodsMap {
 
-        private static Method[] SETCONTENT_METHODS_3x = null;
-        private static Method[] SETCONTENT_METHODS_2x = null;
-        private static Method[] UNSETCONTENT_METHODS_3x = null;
-        private static Method[] UNSETCONTENT_METHODS_2x = null;
-        private static Method[] GETCONTENT_METHODS = null;
+        private static final Method[] SET_CONTENT_METHODS_3x;
+        private static final Method[] SET_CONTENT_METHODS_2x;
+        private static final Method[] UNSET_CONTENT_METHODS_3x;
+        private static final Method[] UNSET_CONTENT_METHODS_2x;
+        private static final Method[] GET_CONTENT_METHODS;
 
         static {
-            SETCONTENT_METHODS_3x = new Method[]{
+            SET_CONTENT_METHODS_3x = new Method[]{
                     ReflectionUtils.findMethod(org.springframework.content.commons.store.ContentStore.class, "setContent", Object.class, PropertyPath.class, InputStream.class, org.springframework.content.commons.store.SetContentParams.class),
                     ReflectionUtils.findMethod(org.springframework.content.commons.store.ContentStore.class, "setContent", Object.class, PropertyPath.class, Resource.class),
             };
 
-            SETCONTENT_METHODS_2x = new Method[]{
+            SET_CONTENT_METHODS_2x = new Method[]{
                     ReflectionUtils.findMethod(ContentStore.class, "setContent", Object.class, PropertyPath.class, InputStream.class, SetContentParams.class),
                     ReflectionUtils.findMethod(ContentStore.class, "setContent", Object.class, PropertyPath.class, Resource.class),
             };
 
-            UNSETCONTENT_METHODS_3x = new Method[]{
+            UNSET_CONTENT_METHODS_3x = new Method[]{
                     ReflectionUtils.findMethod(org.springframework.content.commons.store.ContentStore.class, "unsetContent", Object.class, PropertyPath.class, org.springframework.content.commons.store.UnsetContentParams.class),
             };
 
-            UNSETCONTENT_METHODS_2x = new Method[]{
+            UNSET_CONTENT_METHODS_2x = new Method[]{
                     ReflectionUtils.findMethod(ContentStore.class, "unsetContent", Object.class, PropertyPath.class, UnsetContentParams.class),
             };
 
-            GETCONTENT_METHODS = new Method[]{
+            GET_CONTENT_METHODS = new Method[]{
                     ReflectionUtils.findMethod(ContentStore.class, "getContent", Object.class, PropertyPath.class),
             };
         }
 
 
-        private Class<? extends Store> storeInterface;
-        private PropertyPath path;
-        private Method[] getContentMethods;
-        private Method[] setContentMethods;
-        private Method[] unsetContentMethods;
+        private final Class<? extends Store> storeInterface;
+        private final Method[] getContentMethods;
+        private final Method[] setContentMethods;
+        private final Method[] unsetContentMethods;
 
         public StoreExportedMethodsMap(Class<? extends Store> storeInterface, PropertyPath path, ContentPropertyToExportedContext exportContext) {
             this.storeInterface = storeInterface;
-            this.path = path;
-            this.getContentMethods = calculateExports(GETCONTENT_METHODS, path, exportContext);
+            this.getContentMethods = calculateExports(GET_CONTENT_METHODS, path, exportContext);
             if (org.springframework.content.commons.store.ContentStore.class.isAssignableFrom(storeInterface)) {
-                this.setContentMethods = calculateExports(SETCONTENT_METHODS_3x, path, exportContext);
-                this.unsetContentMethods = calculateExports(UNSETCONTENT_METHODS_3x, path, exportContext);
+                this.setContentMethods = calculateExports(SET_CONTENT_METHODS_3x, path, exportContext);
+                this.unsetContentMethods = calculateExports(UNSET_CONTENT_METHODS_3x, path, exportContext);
             } else {
-                this.setContentMethods = calculateExports(SETCONTENT_METHODS_2x, path, exportContext);
-                this.unsetContentMethods = calculateExports(UNSETCONTENT_METHODS_2x, path, exportContext);
+                this.setContentMethods = calculateExports(SET_CONTENT_METHODS_2x, path, exportContext);
+                this.unsetContentMethods = calculateExports(UNSET_CONTENT_METHODS_2x, path, exportContext);
             }
         }
 
@@ -438,8 +442,7 @@ public class ContentStoreContentService implements ContentService {
 
         private Method[] calculateExports(Method[] storeMethods, PropertyPath path, ContentPropertyToExportedContext exportContext) {
 
-            List<Method> exportedMethods = new ArrayList<>();
-            exportedMethods.addAll(Arrays.asList(storeMethods));
+            List<Method> exportedMethods = new ArrayList<>(Arrays.asList(storeMethods));
 
             List<Method> unexportedMethods = new ArrayList<>();
 
@@ -450,7 +453,7 @@ public class ContentStoreContentService implements ContentService {
                         if (dm.getName().equals(m.getName())) {
                             if (argsMatch(dm, m)) {
                                 RestResource r = dm.getAnnotation(RestResource.class);
-                                if (r != null && r.exported() == false) {
+                                if (r != null && !r.exported()) {
                                     List<String> paths = Arrays.asList(r.paths());
                                     if (paths.contains("*") || paths.contains(path.name())) {
                                         unexportedMethods.add(m);
@@ -493,18 +496,5 @@ public class ContentStoreContentService implements ContentService {
 
             return true;
         }
-    }
-
-    public static boolean isClientAbortException(Exception e) {
-        // prior to Spring Boot 3.2.4
-        if (e.getClass().getSimpleName().equals("ClientAbortException")) {
-            return true;
-            // Spring Boot >= 3.2.4
-        } else if (e instanceof org.springframework.web.context.request.async.AsyncRequestNotUsableException) {
-            if (e.getCause().getClass().getSimpleName().equals("ClientAbortException")) {
-                return true;
-            }
-        }
-        return false;
     }
 }
